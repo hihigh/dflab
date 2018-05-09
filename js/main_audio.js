6f9ffa90b1fb6588eb2b411eb09f561ae2195766
it_work <Poiu0987>
2018-05-09 18:19:45 +0900

    hihigh

--- js/main_audio.js
+++ js/main_audio.js
@@ -1,23 +1,24 @@
-
 /*
+
+/!*
  *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
  *  tree.
- */
+ *!/
 
-/* global AudioContext, SoundMeter */
+/!* global AudioContext, SoundMeter *!/
 
 'use strict';
 
-/*var instantMeter = document.querySelector('#instant meter');
+/!*var instantMeter = document.querySelector('#instant meter');
 var slowMeter = document.querySelector('#slow meter');
 var clipMeter = document.querySelector('#clip meter');
 
 var instantValueDisplay = document.querySelector('#instant .value');
 var slowValueDisplay = document.querySelector('#slow .value');
-var clipValueDisplay = document.querySelector('#clip .value');*/
+var clipValueDisplay = document.querySelector('#clip .value');*!/
 
 var instantValueDisplay = document.querySelector('#instant .value');
 var debug = document.querySelector('#debug .value');
@@ -51,8 +52,8 @@ function handleSuccess(stream) {
         setInterval(function() {
             instantValueDisplay.innerHTML = soundMeter.instant.toFixed(2);
             debug.innerHTML = soundMeter.instant.toFixed(2);
-            /*slowValueDisplay.innerHTML = soundMeter.slow.toFixed(2);
-            clipValueDisplay.innerHTML = soundMeter.clip;*/
+            /!*slowValueDisplay.innerHTML = soundMeter.slow.toFixed(2);
+            clipValueDisplay.innerHTML = soundMeter.clip;*!/
 
             pow += soundMeter.instant.toFixed(2) * 50;
         }, 200);
@@ -102,3 +103,67 @@ update()
 
 
 
+*/
+
+
+
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+/* global AudioContext, SoundMeter */
+
+'use strict';
+
+var instantMeter = document.querySelector('#instant meter');
+var slowMeter = document.querySelector('#slow meter');
+var clipMeter = document.querySelector('#clip meter');
+
+var instantValueDisplay = document.querySelector('#instant .value');
+var slowValueDisplay = document.querySelector('#slow .value');
+var clipValueDisplay = document.querySelector('#clip .value');
+
+try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    window.audioContext = new AudioContext();
+} catch (e) {
+    alert('Web Audio API not supported.');
+}
+
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+    audio: true,
+    video: false
+};
+
+function handleSuccess(stream) {
+    // Put variables in global scope to make them available to the
+    // browser console.
+    window.stream = stream;
+    var soundMeter = window.soundMeter = new SoundMeter(window.audioContext);
+    soundMeter.connectToSource(stream, function(e) {
+        if (e) {
+            alert(e);
+            return;
+        }
+        setInterval(function() {
+            instantMeter.value = instantValueDisplay.innerText =
+                soundMeter.instant.toFixed(2);
+            slowMeter.value = slowValueDisplay.innerText =
+                soundMeter.slow.toFixed(2);
+            clipMeter.value = clipValueDisplay.innerText =
+                soundMeter.clip;
+        }, 200);
+    });
+}
+
+function handleError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+then(handleSuccess).catch(handleError);
